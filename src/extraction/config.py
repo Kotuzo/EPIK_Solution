ba9cfc658e11f51b514ed99f49b630185b4446fd
Norbert Nowak <no.nowak@outlook.com>
2017-11-15 21:14:17 +0100

    Should works

---
+++ src/extraction/config.py
@@ -0,0 +1,12 @@
+# ['id', 'region_id', 'category_id', 'subregion_id', 'district_id',
+#       'city_id', 'accurate_location', 'user_id', 'sorting_date',
+#      'created_at_first', 'valid_to', 'title', 'description',
+#     'full_description', 'has_phone', 'params', 'private_business',
+#    'has_person', 'photo_sizes', 'paidads_id_index', 'paidads_valid_to',
+#   'predict_sold', 'predict_replies', 'predict_views', 'reply_call',
+#  'reply_sms', 'reply_chat', 'reply_call_intent', 'reply_chat_intent',
+# 'parent_id', 'name_pl']
+columnsAds = ['title']
+
+# ['phrase', 'category_id', 'sessions_count', 'name_pl', 'parent_id']
+columnsSearchQueries = ['phrase', 'category_id', 'sessions_count', 'name_pl']
--- src/extraction/extraction.py
+++ src/extraction/extraction.py
@@ -1,43 +1,46 @@
 import pandas as pd
 import os
-from IPython.display import display, HTML
+import config as conf
 
 path = os.getcwd().replace('/src/extraction', '/all_data/').replace("\\src\\extraction", "\\all_data\\")
+extractedPath = path + os.sep + 'extracted'
 
-categories = pd.DataFrame(pd.read_csv(path + os.sep + 'categories.csv'))
-
-
-# .rename(columns={'id': 'category_id'})
+categories = pd.DataFrame(pd.read_csv(path + os.sep + 'categories.csv')).rename(columns={'id': 'category_id'})
 
 
 def searchPathsByStartsWith(startsWith: str):
     paths = []
     for subdir, dir, files in os.walk(path):
         for file in files:
-            filepath = subdir + os.sep + file
-            if file.startswith(startsWith):
-                paths.append(filepath)
+            if not subdir.startswith(extractedPath):
+                    filepath = subdir + os.sep + file
+                    if file.startswith(startsWith):
+                        paths.append(filepath)
     return paths
 
 
-def concatSearchCSV():
+def searchQueriesFromCSV():
     paths = searchPathsByStartsWith('search')
     return pd.DataFrame(pd.concat(
-        [pd.read_csv(p, names=['phrase', 'category_id', 'sessions_count']) for p in paths]))
+        [pd.read_csv(p, names=conf.columnsSearchQueries) for p in paths]))
 
 
-def concatAdsCSV():
+def adsFromCSV():
     paths = searchPathsByStartsWith('ads')
+    print(paths)
     return pd.DataFrame(pd.concat(
         [pd.read_csv(p) for p in paths]))
 
 
+def toCSV(df, columns, filename):
+    if not os.path.exists(extractedPath):
+        os.makedirs(extractedPath)
+    pd.DataFrame(df[columns]).to_csv(extractedPath + os.sep + filename + '.csv')
+
+
 def main():
-    # searchQueries = concatSearchCSV()
-    ads = concatAdsCSV()
-    # test = searchQueries.merge(categories, left_on='category_id', right_on='parent_id', how='left', suffixes=('_sq', '_cat'))
-    test1 = ads.merge(categories, left_on='category_id', right_on='id', how='inner', suffixes=('_ads', '_cat'))
-    print(test1[['id_ads', 'id_cat']])
+    toCSV(pd.merge(searchQueriesFromCSV(), categories, on='category_id'), conf.columnsSearchQueries, 'search')
+    toCSV(pd.merge(adsFromCSV(), categories, on='category_id'), conf.columnsAds, 'ads')
 
 
 if __name__ == '__main__':
