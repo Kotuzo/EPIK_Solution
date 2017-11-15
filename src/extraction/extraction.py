80d5080939dad6d8950fe657223bda0b42a458bc
Norbert Nowak <no.nowak@outlook.com>
2017-11-15 19:33:01 +0100

    idk why merge doesnt work

--- src/extraction/extraction.py
+++ src/extraction/extraction.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import os
-import glob
+from IPython.display import display, HTML
 
 path = os.getcwd().replace('/src/extraction', '/all_data/')
 
-categories = pd.DataFrame(pd.read_csv(path + os.sep + 'categories.csv', error_bad_lines=False))
+categories = pd.DataFrame(pd.read_csv(path + os.sep + 'categories.csv')).rename(columns={'id': 'category_id'})
 
 
 def searchPathsByStartsWith(startsWith: str):
@@ -17,16 +17,29 @@ def searchPathsByStartsWith(startsWith: str):
     return paths
 
 
-# TODO remove :5 from loop
-def concatCSV(paths):
-    return pd.concat([pd.read_csv(p, error_bad_lines=False) for p in paths[:5]])
+def concatSearchCSV():
+    paths = searchPathsByStartsWith('search')
+    return pd.DataFrame(pd.concat(
+        [pd.read_csv(p, names=['phrase', 'category_id', 'sessions_count']) for p in paths[:10]]))
+
+
+def concatAdsCSV():
+    paths = searchPathsByStartsWith('ads')
+    return pd.DataFrame(pd.concat(
+        [pd.read_csv(p) for p in paths]))
 
 
 def main():
-    searchQueries = pd.DataFrame(concatCSV(searchPathsByStartsWith('search'))).rename(
-        columns={'Unnamed: 0': 'phrase', 'Unnamed: 1': 'category_id'})[['phrase', 'category_id']]
-    print(searchQueries.keys())
-    print(searchQueries)
+    searchQueries = concatSearchCSV()
+    # print(searchQueries)
+    # print(searchQueries.keys())
+    # print(categories.keys())
+    test = searchQueries.merge(categories, on='category_id', how='inner', suffixes=('_sq', '_cat'))
+    print(test.keys())
+    print(test)
+    # print(test[['category_id_sq', 'category_id_cat']])
+    test.to_csv(
+        path + os.sep + 'test.csv', index=False)
 
 
 if __name__ == '__main__':
