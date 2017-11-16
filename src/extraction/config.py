093ea6c319476b0a2f9ae477db554de87db0ecf4
Norbert Nowak <norbert.nowak@rsqtechnologies.com>
2017-11-17 00:12:20 +0100

    Working extraction with dates TODO: how to merge ads with searchqueries when one (ads) to many(searchQueries)

--- src/extraction/config.py
+++ src/extraction/config.py
@@ -1,13 +1,13 @@
-# ['id', 'region_id', 'category_id', 'subregion_id', 'district_id',
-#       'city_id', 'accurate_location', 'user_id', 'sorting_date',
-#      'created_at_first', 'valid_to', 'title', 'description',
-#     'full_description', 'has_phone', 'params', 'private_business',
-#    'has_person', 'photo_sizes', 'paidads_id_index', 'paidads_valid_to',
-#   'predict_sold', 'predict_replies', 'predict_views', 'reply_call',
-#  'reply_sms', 'reply_chat', 'reply_call_intent', 'reply_chat_intent',
-# 'parent_id', 'name_pl']
-columnsAds = ['title', 'name_pl']
+ # ['id', 'region_id', 'category_id', 'subregion_id', 'district_id',
+ #       'city_id', 'accurate_location', 'user_id', 'sorting_date',
+ #       'created_at_first', 'valid_to', 'title', 'description',
+ #       'full_description', 'has_phone', 'params', 'private_business',
+ #       'has_person', 'photo_sizes', 'paidads_id_index', 'paidads_valid_to',
+ #       'predict_sold', 'predict_replies', 'predict_views', 'reply_call',
+ #       'reply_sms', 'reply_chat', 'reply_call_intent', 'reply_chat_intent']
+columnsAds = ['sorting_date', 'category_id', 'title']
+# ['sorting_date', 'phrase', 'category_id', 'sessions_count', 'parent_id', 'name_pl']
+columnsSearchQueries = ['sorting_date',  'category_id',  'name_pl', 'sessions_count']
 
-# ['phrase', 'category_id', 'sessions_count', 'name_pl', 'parent_id']
-columnsSearchQueries = ['phrase', 'category_id', 'sessions_count', 'name_pl']
-columnNamesSearchQueries = ['phrase', 'category_id', 'sessions_count']
+finalColumns = ['sessions_count', 'title', 'name_pl', 'sorting_date', 'phrase']
+staticColumnsSearchQueries= ['sorting_date', 'phrase', 'category_id', 'sessions_count']
--- src/extraction/extraction.py
+++ src/extraction/extraction.py
@@ -6,62 +6,74 @@ path = os.getcwd().replace('/src/extraction', '/all_data/').replace("\\src\\extr
 extractedPath = path + 'extracted'
 categories = pd.DataFrame(pd.read_csv(path + os.sep + 'categories.csv')).rename(columns={'id': 'category_id'})
 
-
-def searchPathsByStartsWith(startsWith: str):
+def findPathsByStartsWith(startsWith):
     paths = []
     for subdir, dir, files in os.walk(path):
         for file in files:
             if not subdir.startswith(extractedPath):
                 filepath = subdir + os.sep + file
-                if file.startswith(startsWith):
+                if (file.startswith(startsWith)):
                     paths.append(filepath)
     return paths
 
 
-def repairFile(paths):
-    for path in paths:
-        tempString = ''
-        with open(path, 'r', encoding='utf-8') as f:
-            for line in f:
-                commaIndexes = findIndexes(line, ',')
-                if len(commaIndexes) > 2:
-                    for i in commaIndexes[:-2]:
-                        temp = list(line)
-                        temp[i] = '.'
-                        line = "".join(temp)
-                tempString += line
-        with open(path[:-4] + 'oczko' + '.csv', 'w', encoding='utf-8') as f:
-            f.write(tempString)
-
-
-def findIndexes(s, ch):
+def findCharIndexesInString(s, ch):
     return [i for i, ltr in enumerate(s) if ltr == ch]
 
 
-def searchQueriesFromCSV():
-    paths = searchPathsByStartsWith('search')
-    print('\n'.join(paths[1:2]))
+def convertSearchQueriesCSVToDataFrame():
+    paths = findPathsByStartsWith('search')
     return pd.DataFrame(pd.concat(
-        [pd.read_csv(p, names=conf.columnNamesSearchQueries) for p in paths[1:2]]))
+        [pd.read_csv(p, names=conf.staticColumnsSearchQueries) for p in paths]))
 
 
-def adsFromCSV():
-    paths = searchPathsByStartsWith('ads')
+def convertAdsCSVToDataFrame():
+    paths = findPathsByStartsWith('ads')
     return pd.DataFrame(pd.concat(
         [pd.read_csv(p) for p in paths]))
 
 
-def toCSV(df, columns, filename):
+def convertDataFrameToCSV(df, columns, filename):
     if not os.path.exists(extractedPath):
         os.makedirs(extractedPath)
     pd.DataFrame(df[columns]).to_csv(extractedPath + os.sep + filename + '.csv')
 
 
+def prepareSearchQueriesCSV(paths):
+    for path in paths:
+        tempString = ''
+        with open(path, 'r', encoding='utf-8') as f:
+            date = "\"" + f.name[-14:-4] + "\","
+            date = date.replace('_', '-')
+            for line in f:
+                commaIndexes = findCharIndexesInString(line, ',')
+                if not line.startswith(date):
+                    tempString += date
+                    if len(commaIndexes) > 2:
+                        for i in commaIndexes[:-2]:
+                            temp = list(line)
+                            temp[i] = '.'
+                            line = "".join(temp)
+                tempString += line
+        with open(path, 'w', encoding='utf-8') as f:
+            f.write(tempString)
+
+
+def removeTimeFromDateTime(df, columnName):
+    return pd.to_datetime(df[columnName]).dt.date
+
+
 def main():
-    repairFile(searchPathsByStartsWith('search'))
-    # print(searchQueriesFromCSV().iloc[122990])
-    # toCSV(pd.merge(searchQueriesFromCSV(), categories, on='category_id'), conf.columnsSearchQueries, 'search')
-    # toCSV(pd.merge(adsFromCSV(), categories, on='category_id'), conf.columnsAds, 'ads')
+    prepareSearchQueriesCSV(findPathsByStartsWith('search'))
+    searchQueriesCategories = pd.merge(convertSearchQueriesCSVToDataFrame(), categories, on='category_id')
+    ads = convertAdsCSVToDataFrame()
+    ads['sorting_date'] = removeTimeFromDateTime(ads, 'sorting_date')
+
+    convertDataFrameToCSV(searchQueriesCategories, conf.columnsSearchQueries, 'search')
+    convertDataFrameToCSV(ads, conf.columnsAds, 'ads')
+
+    # finalDataFrame = pd.merge(ads, searchQueriesCategories, on=['sorting_date', 'category_id'])
+    # convertDataFrameToCSV(finalDataFrame, conf.finalColumns, 'final')
 
 
 if __name__ == '__main__':
